8dc1faae395345ea877b05ccd40445ae4faf271a
libp <924701358@qq.com>
2021-09-21 22:14:16 +0800

    完成首页基础样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,45 +4,55 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    timeArray: ['秒','分钟','小时'],
+    index: 0,
+    inputValue: '',
+    isReEnter: true,
   },
-  // 事件处理函数
-  bindViewTap() {
+  Focus(e){
+    console.log(e.detail.value);
+    this.setData({
+      inputValue:e.detail.value,
+    })
+    if(e.detail.value.length==6){
+      console.log("超过最大输入长度： " + e.detail.value.length);
+      // this.setData({
+      //   inputValue: '',
+      //   index: '1',
+      //   isReEnter: false,
+      // })
+    }
+  },
+  bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindKeyInput: function() {
+    var time = this.data.inputValue;
+    var timescale = this.data.index
+    switch ( timescale ) {
+      case 1:
+        time = time * 60000;
+        break;
+      case 2:
+        time = time * 3600000;
+        break;
+      default:
+        time = time * 1000;
+    }
+    console.log('开始倒计时 ' + time);
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../logs/logs'+'?timer='+time,
+    })
+  },
+
+  bindshortcut: function(e) {
+    wx.navigateTo({
+      url: '../logs/logs'+'?timer='+e.target.dataset.field,
     })
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -5,14 +5,9 @@ Page({
   data: {
     logs: []
   },
-  onLoad() {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
-      })
-    })
+  onLoad(res) {
+    // 读取当前页面信息
+    console.log(res)
+
   }
 })
