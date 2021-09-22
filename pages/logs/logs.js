acf808771728436064ff1b7cffeb1e886165488e
libp <924701358@qq.com>
2021-09-22 21:34:53 +0800

    添加倒计时页面样式

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,13 +1,33 @@
 // logs.js
-const util = require('../../utils/util.js')
+const Egg = require('../../utils/egg.js')
+const Time = require('../../utils/time.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    Width:100,
+    Height:100
   },
   onLoad(res) {
+    var that = this;
+    wx.getSystemInfo({
+      success (res) {
+        that.setData({
+          Width: res.windowWidth-20,
+          Height: res.windowHeight-20
+        })
+      }
+    })
+
     // 读取当前页面信息
-    console.log(res)
+    console.log(Egg)
+    console.log(Time)
+
+    Egg.startTime = (new Date()).getTime();
+    var duration = parseInt(res.timer)
+    Egg.endTime = Egg.startTime + duration
+    console.log(Egg)
+    Egg.start();
 
   }
 })
